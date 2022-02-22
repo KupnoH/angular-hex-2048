59cfdb892c27874fd2b135134d10666f91c67505
Artyom Ilyin <arilyin@comscore.com>
2022-02-22 15:45:43 +0300

    Added basic hexagonal grid building, added styling to have hexagonal cells instead of circles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HexDataType, HexRowType } from "./types/hexDataTypes";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'hex2048';
+  radius: number = 3;
+  hexData: HexDataType = [];
+
+  ngOnInit() {
+    this.buildHexGrid(this.radius);
+  }
+
+  /**
+   * Function to build hexagonal grid data.
+   *
+   * @param r - grid radius,
+   */
+  buildHexGrid(r: number) {
+    // Build the first part of the grid.
+    for (let i = 0; i < r; i++) { // i represents column.
+      let row: HexRowType = [];
+      for (let j = 0; j < (r + i); j++) { // j represents cell.
+        // Calculate coordinates and create element and add it to row.
+        row.push({
+          x: i - j,
+          y: r - i - 1,
+          z: -(r - j - 1),
+          value: 0,
+        });
+      }
+      this.hexData.push(row);
+    }
+
+    // Build the second part.
+    for (let i = r - 1; i > 0; i--) { // i represents column.
+      let row = [];
+      for (let j = 0; j < (r + i - 1); j ++) { // j - represents cell.
+        // Calculate coordinates and create element and add it to row.
+        row.push({
+          x: r - j - 1,
+          y: -(r - i),
+          z: -(i - j - 1),
+          value: 0,
+        });
+      }
+      this.hexData.push(row);
+    }
+  }
 }
